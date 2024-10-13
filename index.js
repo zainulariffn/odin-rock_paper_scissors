a49d486e5817f4b4eaa26067f19276dd4d834b40
Zainul Rashid <56968419+zainulariffn@users.noreply.github.com>
2024-10-13 14:29:55 +0800

    Add description for codes

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-console.log("Hello zainul!");
+// My requirement
+// keep track of scores
+// Best of 5, first to 3 wins
+// declare winner
+
+console.log("Welcome to rock paper and scissors!");
 
 // 2
-
 function getComputerChoice() {
+  // Get random choice for computer (rock, paper, scissors)
   let computerRandom = Math.floor(Math.random() * 3);
   if (computerRandom == 0) {
     return "rock";
@@ -15,9 +20,8 @@ function getComputerChoice() {
 
 // 3
 function getHumanChoice() {
-  let answer = prompt(
-    "Lets play a game, 0-Rock, 1-Paper, 2-Scissors"
-  ).toLowerCase();
+  // Get human choice through prompt
+  let answer = prompt("Lets play rock, paper, scissors").toLowerCase();
 
   if (answer == "rock") {
     return "rock";
@@ -30,45 +34,65 @@ function getHumanChoice() {
   }
 }
 
-// 4
-let humanScore = 0;
-let computerScore = 0;
-
-// 5
-
-function playRound(humanChoice, computerChoice) {
-  if (computerChoice === humanChoice) {
-    console.log("Its a tie!");
-  } else if (computerChoice === "paper" && humanChoice === "rock") {
-    console.log("Computer wins, paper beats rock");
-    return computerScore++;
-  } else if (computerChoice === "scissors" && humanChoice === "paper") {
-    console.log("Computer wins, scissors beats paper");
-    return computerScore++;
-  } else if (computerChoice === "rock" && humanChoice === "scissors") {
-    console.log("Computer wins, rock beats scissors");
-    return computerScore++;
-  } else if (humanChoice === "paper" && computerChoice === "rock") {
-    console.log("Player wins, paper beats rock");
-    return humanScore++;
-  } else if (humanChoice === "scissors" && computerChoice === "paper") {
-    console.log("Player wins, scissors beats paper");
-    return humanScore++;
-  } else if (humanChoice === "rock" && computerChoice === "scissors") {
-    console.log("Player wins, rock beats scissors");
-    return humanScore++;
-  }
-}
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-console.log(computerSelection);
-playRound(humanSelection, computerSelection);
-console.log(computerSelection);
-
 // 6
 function playGame() {
-  // call play round 5 times
+  // Play the game for best of 5, who gets 3 rounds wins
+
+  // Odin requirement
+  // Your game will play 5 rounds.
+  // You will write a function named playGame that calls playRound to play 5 rounds,
+  // keeps track of the scores and declares a winner at the end.
+
+  // 4
+  let humanScore = 0;
+  let computerScore = 0;
+
+  // Logs the winner
+  if (humanScore >= 3) {
+    console.log("Player wins!");
+  } else if (computerScore >= 3) {
+    console.log("Computer wins!");
+  }
+
+  // 5
+  function playRound(humanChoice, computerChoice) {
+    // Get round winners
+    if (computerChoice === humanChoice) {
+      console.log("Its a tie!");
+    } else if (computerChoice === "paper" && humanChoice === "rock") {
+      console.log("Computer wins, paper beats rock");
+      return computerScore++;
+    } else if (computerChoice === "scissors" && humanChoice === "paper") {
+      console.log("Computer wins, scissors beats paper");
+      return computerScore++;
+    } else if (computerChoice === "rock" && humanChoice === "scissors") {
+      console.log("Computer wins, rock beats scissors");
+      return computerScore++;
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+      console.log("Player wins, paper beats rock");
+      return humanScore++;
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+      console.log("Player wins, scissors beats paper");
+      return humanScore++;
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+      console.log("Player wins, rock beats scissors");
+      return humanScore++;
+    }
+  }
+  const humanSelection = getHumanChoice();
+  const computerSelection = getComputerChoice();
+
+  // Log chocies for human and computer
+  console.log("Human choose " + humanSelection);
+  console.log("Computer choose " + computerSelection);
+  // Play each round using humanSelection function and computerFunction as argument for playRound function
+  playRound(humanSelection, computerSelection);
+  // Logs the score round for computer and human
+  console.log("Human score is " + humanScore);
+  console.log("Computer score is " + computerScore);
 }
+// This is wrong and i dont know how to create playGame function
+// Play the game for 5 round
 playGame();
 playGame();
 playGame();
